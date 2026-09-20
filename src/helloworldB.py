816fcf8d4ae5273535e36ac108a537487733b7e5
Ivan <mailivanreyes@gmail.com>
2026-09-20 18:28:14 +1000

    hello bro

---
+++ src/helloworldB.py
@@ -0,0 +1,9 @@
+#python3 helloworld.py
+
+def greet():
+    return "Hello Bro!"
+
+if __name__ == "__main__":
+    print(greet()) 
+
+
--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -1,5 +1,8 @@
 from helloworld import greet
+from helloworldB import greet as #greetBeta #testing a second file with an identical function
 
 def test_greet_returns_message():
     assert greet() == "Hello World!"
+    assert greetBeta() == "Hello World!" #second file
+
 
